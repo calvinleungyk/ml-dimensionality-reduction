937bf946c7078fd770a9459e648f1ea89514cffe
Calvin Leung <calvinleungyk@gmail.com>
2016-10-08 02:42:16 -0700

    implemented fastmap without visualization

--- pca.py
+++ pca.py
@@ -3,247 +3,129 @@ import math
 import copy
 import random
 
-class Clustering:
-	def __init__(self, k, list_of_points):
-		self.points_space = list_of_points
-		self.k = k
-		self.clusters = list()
-		self.centroids = None
-		self.previous_centroids = None
-		self.min_distance_from_centroids = sys.maxsize
-		self.best_clusters = list()
-		self.best_centroids = list()
-		self.mu = list()
-		self.sigma = list()
-		self.pi = list()
-		self.responsibilities = list()
+class PCA:
+	def __init__(self):
+		self.something = list()
 
-	def sum_distance_from_centroids(self):
-		sum_distance = 0
-		for (cluster, centroid) in zip(self.clusters, self.centroids):
-			for point in cluster.points:
-				sum_distance += point.sum_of_squares(centroid)
-		return sum_distance
+	def read_data(self):
+		return
 
-	def no_change_in_centroids(self):
-		sum_distance = 0
-		for (old, new) in zip(self.previous_centroids, self.centroids):
-			sum_distance += old.distance_to(new)
-		if sum_distance != 0:
-			return False
+class Fastmap:
+	def __init__(self):
+		self.target_space = list()
+		for i in range(10):
+			temp = list()
+			for j in range(2):
+				temp.append(0)
+			self.target_space.append(temp)
+
+		self.object_space = list()
+		
+		for i in range(10):
+			temp = list()
+			for j in range(10):
+				temp.append(0)
+			self.object_space.append(temp)
+		self.o_a = -1
+		self.o_b = -1
+		self.curr_col = -1
+
+
+	# reads a file and returns a list of points
+	def read_data(self):
+		with open('fastmap-data.txt', 'r') as rawdata:
+			lines = rawdata.readlines()
+			for line in lines:
+				if line != '\n':
+					obj1, obj2, distance = (int(s) for s in line.split())
+					self.object_space[obj1-1][obj2-1] = distance
+					self.object_space[obj2-1][obj1-1] = distance
+
+	def choose_furthest_objects(self):
+		o_a = 1
+		o_b = -1
+		max_distance = -1
+		for i in range (6):
+			for j in range(10):
+				if self.object_space[o_a-1][j] > max_distance:
+					o_b = j+1
+					max_distance = self.object_space[o_a-1][j]
+			temp = o_a
+			o_a = o_b
+			o_b = temp
+		self.o_a = o_a
+		self.o_b = o_b
+		print (self.o_a, self.o_b)
+
+	def hyperplane_furthest_objects(self):
+		o_a = 1
+		o_b = 2
+		max_distance = -1
+		for i in range (6):
+			for j in range(10):
+				if self.hyperplane_distance(o_a, j+1) > max_distance:
+					o_b = j+1
+					max_distance = self.hyperplane_distance(o_a, j+1)
+			temp = o_a
+			o_a = o_b
+			o_b = temp
+		self.o_a = o_a
+		self.o_b = o_b
+		print (self.o_a, self.o_b)
+
+	# o_i index is from 1-10
+	def individual_projected_distance(self, o_i):
+		return ( (self.object_space[o_i-1][self.o_a-1]**2 + 
+			self.object_space[self.o_b-1][self.o_a-1]**2 - 
+			self.object_space[o_i-1][self.o_b-1]**2) / 
+			(2*self.object_space[self.o_b-1][self.o_a-1]))
+
+	def hyperplane_projected_distance(self, o_i):
+		return ( (self.hyperplane_distance(self.o_a, o_i)**2 + 
+			self.hyperplane_distance(self.o_a, self.o_b)**2 - 
+			self.hyperplane_distance(self.o_b, o_i)**2) / 
+			(2*self.hyperplane_distance(self.o_a, self.o_b)))
+
+	def hyperplane_distance(self, o_x, o_y):
+		return math.sqrt( (self.object_space[o_x-1][o_y-1])**2 - (self.target_space[o_x-1][self.curr_col-1] - self.target_space[o_y-1][self.curr_col-1])**2 )
+
+	def set_column_to_zero(self):
+		for i in range(10):
+			self.target_space[i][self.curr_col] = 0
+
+	def fastmap(self, k):
+		if k <= 0:
+			for i in range (10):
+				print (self.target_space[i])
+			print ("return 1")
+			return
 		else:
-			return True
+			self.curr_col += 1
 
-	def random_centroids(self):
-		centroids = list()
-		while len(centroids) < 3:
-			point = self.points_space[random.randint(0,len(self.points_space)-1)]
-			if (point not in centroids):
-				centroids.append(point)
-		return centroids
+		if self.curr_col == 0:
+			self.choose_furthest_objects()
+			if self.object_space[self.o_a-1][self.o_b-1] == 0:
+				self.set_column_to_zero()
+				for i in range (10):
+					print (self.target_space[i])
+				print ("return 2")
+				return
+			for i in range(10):
+				self.target_space[i][self.curr_col] = self.individual_projected_distance(i+1)
+		else:
+			self.hyperplane_furthest_objects()
+			if self.hyperplane_distance(self.o_a, self.o_b) == 0:
+				self.set_column_to_zero()
+				for i in range (10):
+					print (self.target_space[i])
+				print ("return 3")
+				return
+			for i in range(10):
+				self.target_space[i][self.curr_col] = self.hyperplane_projected_distance(i+1)
+		self.fastmap(k-1)
 
-	def assignment(self):
-		self.clear_points_from_clusters()
-		modified = False
-		for point in self.points_space:
-			min_distance = 10000
-			closest_centroid_index = -1
-			for i, centroid in enumerate(self.centroids):
-				current_distance = point.distance_to(self.centroids[i])
-				if (current_distance < min_distance):
-					min_distance = point.distance_to(self.centroids[i])
-					closest_centroid_index = i
-			self.clusters[closest_centroid_index].add_point(point)
-		return
-
-	def clear_points_from_clusters(self):
-		for cluster in self.clusters:
-			cluster.clear_points()
-
-	def update(self):
-		new_centroids = list()
-		for cluster in self.clusters:
-			total_x = 0
-			total_y = 0
-			num_points_in_cluster = len(cluster.points)
-			for point in cluster.points:
-				total_x += point.x
-				total_y += point.y
-			new_x = total_x / num_points_in_cluster
-			new_x = round(new_x, 9)
-			new_y = total_y / num_points_in_cluster
-			new_y = round(new_y, 9)
-			new_centroids.append(Point(new_x, new_y))
-		self.previous_centroids = self.centroids
-		self.centroids = new_centroids
-		return
-
-	def k_means(self):
-		no_change = False
-		self.centroids = self.random_centroids()
-		for i, centroid in enumerate(self.centroids):
-			self.clusters.append(Cluster(i, centroid))
-		for i in range (0, 1000):	
-			while not no_change:
-				self.assignment()
-				self.update()
-				if self.no_change_in_centroids():
-					break
-			sum_distance_from_centroids = self.sum_distance_from_centroids()
-			if  sum_distance_from_centroids < self.min_distance_from_centroids:
-				self.min_distance_from_centroids = sum_distance_from_centroids
-				self.best_centroids = copy.deepcopy(self.centroids)
-				self.best_clusters = copy.deepcopy(self.clusters)
-		print ("The best centroids after 1000 runs are: ", self.best_centroids)
-		print ("Number of points in clusters: ", len(self.best_clusters[0].points), len(self.best_clusters[1].points), len(self.best_clusters[2].points))
-		return
-
-	def initialize_gmm(self):
-		# kmeans for initializing
-		self.mu = self.best_centroids
-		self.sigma = []
-		for i, cluster in enumerate(self.best_clusters):
-			xx = 0
-			xy = 0
-			yx = 0
-			yy = 0
-			Xs = []
-			Ys = []
-			for point in cluster.points:
-				Xs.append(point.x - self.best_centroids[i].x)
-				Ys.append(point.y - self.best_centroids[i].y)
-			for (x, y) in zip(Xs, Ys):
-				xx += x**2
-				xy += x*y
-				yy += y**2
-			yx = xy
-			cluster_size = len(cluster.points)
-			self.sigma.append([[xx/cluster_size, xy/cluster_size],[yx/cluster_size, yy/cluster_size]])
-
-		self.pi = [len(self.best_clusters[i].points)/150 for i in range(self.k)]
-
-	def gaussian(self, cluster_num, point):
-		covariance_matrix = self.sigma[cluster_num]
-		a = covariance_matrix[0][0]
-		b = covariance_matrix[0][1]
-		c = covariance_matrix[1][0]
-		d = covariance_matrix[1][1]
-		sigma_determinant =  a * d  -  c * b
-		sigma_inverse = [[d/ sigma_determinant, -b/sigma_determinant], [-c/sigma_determinant, a/sigma_determinant]]
-		x = point.x - self.best_centroids[cluster_num].x
-		y = point.y - self.best_centroids[cluster_num].y
-		exponent = x*x*sigma_inverse[0][0] + y*x*sigma_inverse[1][0] + x*y*sigma_inverse[0][1] + y*y*sigma_inverse[1][1]
-
-		denominator = ( (2*math.pi) * math.sqrt(sigma_determinant) )
-		numerator = math.exp(-1/2* (exponent) )
-		return (numerator / denominator)
-
-	def e_step(self):
-		self.responsibilities = []
-		for point in self.points_space:
-			denominator = 0
-			point_responsibility = []
-			for i in range (k):
-				denominator += self.pi[i] * self.gaussian(i, point)
-
-			for i in range (k):
-				point_responsibility.append(self.pi[i] * self.gaussian(i, point)/ denominator)
-			self.responsibilities.append(point_responsibility)
-			temp = 0
-		for i in range(150):
-			for j in range (3):
-				temp += self.responsibilities[i][j]
-		return
-
-	def m_step_covar_helper(self, i, pi):
-		xx = 0
-		xy = 0
-		yx = 0
-		yy = 0
-		Xs = []
-		Ys = []
-		for (j, point) in enumerate(self.best_clusters[i].points):
-			point_responsibility = self.responsibilities[j][i]
-			Xs.append(point_responsibility*(point.x - self.best_centroids[i].x))
-			Ys.append(point_responsibility*(point.y - self.best_centroids[i].y))
-		for (x, y) in zip(Xs, Ys):
-			xx += x**2
-			xy += x*y
-			yy += y**2
-			yx += y*x
-		return ([[xx/pi, xy/pi],[yx/pi, yy/pi]])
-
-	def m_step(self):
-		self.mu = []
-		self.sigma = []
-		self.pi = []
-		for i in range (k):
-			mu = [0,0]
-			pi = 0
-			for (j, point) in enumerate(self.points_space):
-				point_responsibility = self.responsibilities[j][i]
-				mu[0] += point_responsibility*point.x
-				mu[1] += point_responsibility*point.y
-				pi += point_responsibility
-				#print (i, j)
-				#print (pi)
-			mu[0] /= pi
-			mu[1] /= pi
-			self.mu.append(mu)
-			self.sigma.append(self.m_step_covar_helper(i, pi))
-			self.pi.append(pi/150)
-
-	def gmm(self):
-		self.initialize_gmm()
-		self.e_step()
-		self.m_step()
-		print ("Gaussian 1: mean, amplitude, covariance", self.mu[0], self.pi[0], self.sigma[0])
-		print ("Gaussian 2: mean, amplitude, covariance", self.mu[1], self.pi[1], self.sigma[1])
-		print ("Gaussian 3: mean, amplitude, covariance", self.mu[2], self.pi[2], self.sigma[2])
+fastmap = Fastmap()
+fastmap.read_data()
+fastmap.fastmap(2)
 
 
-class Cluster:
-	def __init__(self, id, centroid):
-		self.id = id
-		self.points = list()
-		self.centroid = centroid
-	
-	def add_point(self, point):
-		self.points.append(point)
-
-	def clear_points(self):
-		self.points.clear()
-
-class Point:
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
-		self.cluster = None
-
-	def __repr__(self):
-		return str(self.x) + " " + str(self.y)
-
-	def distance_to(self, another_point):
-		return math.sqrt( (another_point.x - self.x)**2 + (another_point.y - self.y)**2 )
-
-	def sum_of_squares(self, another_point):
-		return (another_point.x - self.x)**2 + (another_point.y - self.y)**2
-
-# reads a file and returns a list of points
-def read_data():
-	list_of_points = []
-	with open('clusters.txt', 'r') as rawdata:
-		lines = rawdata.readlines()
-		for line in lines:
-			if line != '\n':
-				line.rstrip()
-				raw_point = line.split(',')
-				int_point = Point(float(raw_point[0]), float(raw_point[1]))
-				list_of_points.append(int_point)
-	return list_of_points
-
-k = 3
-list_of_points = read_data()
-clustering = Clustering(k, list_of_points)
-clustering.k_means()
-clustering.gmm()
